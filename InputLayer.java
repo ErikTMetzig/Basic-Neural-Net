fb3fd96f43d1618e2d79788db2be3b47bdc3497c
Erik Metzig <e.metzig@gmail.com>
2017-07-24 23:09:32 -0500

    Update InputLayer.java

--- InputLayer.java
+++ InputLayer.java
@@ -3,9 +3,74 @@
 */
 package neuralnet;
 
-public class InputLayer extends NeuralLayer {
+import java.util.*
+
+public class InputLayer extends Layer {
+  
   //only one parameter because it's the first layer
   public InputLayer(int numberofinputs) {
+    
+    super(numberofinputs, new Lienar(1));
+    previousLayer = null;
     this.numberOfInputs = numberofinputs;
+    init();
+    
+  }
+  
+  /*
+  * void setPreviousLayer - Doesn't allow a previous layer to be set
+  */
+  @Override
+  public void setPreviousLayer(NeuralLayer layer) {
+    previousLayer = null;
+  }
+  
+  /*
+  * setNextLayer - links this layer to the next one
+  */
+  @Override
+  public void setNextLayer(NeuralLayer layer) {
+    layer.setPreviousLayer(this);
+    this.nextLayer = layer;
+  }
+
+  /*
+  * void calculate - passes the input values to the outputs because this
+  * is the first layer
+  */
+  @Override
+  public void calculate() {
+  
+    if(getListOfNeurons() != null && this.input != null)  {
+    
+      for(int i = 0; i < numberOfNeuronsInLayer; i++) {
+      
+        double[] input1 = {this.input.get(i)};
+        getNeuron(i).setInputs(input1);
+        getNeuron(i).calculate();
+        
+        try {
+          otuput.set(i, getNeuron(i).getOutput())
+        }
+        
+        catch(IndexOutOfBoundsException iobe) {
+        
+          otuput.add(getNeuron(i).getOutput());
+        }
+      }
+    }
+  }
+  
+  /*
+  * void setInput - pass an array of vals in to the inputs
+  */
+  @Override
+  public void setInput(ArrayList<Double> input) {
+  
+    if(input.size() == numberOfInputs)  {
+      this.input = input;
+      
+    }
   }
 }
+       
