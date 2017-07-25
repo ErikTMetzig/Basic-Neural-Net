f477385fe04601b52eb9370d738ee5fc9c93f692
Erik Metzig <e.metzig@gmail.com>
2017-07-25 18:40:50 -0500

    Update NeuralNet.java
    Cleaned up the primary constructor function

--- NeuralNet.java
+++ NeuralNet.java
@@ -1,5 +1,6 @@
 /*
-* Represents an entire neural network, containing data for number of layers and inputs
+* Represents an entire neural network, containing data for number of layers and inputs.
+* Oversees all necessary functions for the operation of a neural net
 */
 package neuralnet;
 
@@ -7,12 +8,18 @@ import java.util.*;
 
 public class NeuralNet  {
 
+  //if the neural net will have a bias
+  private bool hasBias = true;
+  
   //first layer
   private InputLayer inputLayer;
+  
   //all of the middle layers
-  private ArrayList<HiddenLayer> hiddenLayer;
+  private ArrayList<MiddleLayer> middleLayer;
+  
   //the final layer
   private OutputLayer outputLayer;
+  
   //int to track size of middle layer array
   private int numberOfHiddenLayers;
   private int numberOfInputs;
@@ -20,32 +27,63 @@ public class NeuralNet  {
   private ArrayList<Double> input;
   private ArrayList<Double> output;
   
+  
   /*
   * Constructor - used to establish all aspects of the neural net
   */
-  public NeuralNet(int numberofinputs, int numberofoutputs, int[] numberofhiddenneurons, IActivationFunction[] hiddenAcFnc, IActivationFunction outputAcFnc)  {
+  public NeuralNet(int numinputs, int numoutputs, int[] nummiddleneurons, Activation[] hiddenAcFnc, Activation outputAcFnc)  {
+    
     //set appropriate vars
-    this.numberOfInputs = numberofinputs;
-    this.numberOfOutputs = numberofoutputs;
-    //initialize inputs
-    input = new ArrayList<>(numberofinputs);
-    inputLayer = new InputLayer(numberofinputs);
-    //initialize the first hidden layer
-    hiddenLayer.set(0, new HiddenLayer(numberofhiddenneurons[0], hiddenAcFnc[0], inputLayer.getNumberOfNeuronsInLayer()));
-    inputLayer.setNextLayer(hiddenLayer.get(0));
-    //initialize all of the other hidden layers by iteration
-    for(int i = 1; i < hiddenLayer.size(); i++) {
-      hiddenLayer.set(i, new HiddenLayer(numberofhiddenneurons[i], hiddenAcFnc[i], hiddenLayer.get(i - 1).getNumberOfNeuronsInLayer()));
-      hiddenLayer.get(i - 1).setNextLayer(hiddenLayer.get(i));
+    this.numberOfInputs = numinputs;
+    this.numberOfOutputs = numoutputs;
+    numberOfHiddenLayers = numberofhiddenneurons.length;
+    
+    if(numberOfHiddenLayers == hiddenAcFnc.length)  {
+      
+      inputLayer = new InputLayer(numinputs);
+      input = new ArrayList<>(numinputs);
+      
+      if(this.numberOfMiddleLayers > 0)
+        middleLayer = new ArrayList<>(numberOfMiddleLayers);
+   
+      try {
+        
+        middleLayer.set(0, new MiddleLayer(nummiddleneurons[0], hiddenAcFnc[0], inputLayer.getNumberOfNeuronsInLayer()));
+      }
+      
+      catch(IndexOutOfBoundsException iobe) {
+        
+        middleLayer.add(new MiddleLayer(nummiddleneurons[0], hiddenAcFnc[0], inputLayer.getNumNeurons()));
+      }
+      
+      inputLayer.setNextLayer(middleLayer.get(0));
+      
+      for(int i = 1; i < numMiddleLayers; i++)  {
+        
+        try {
+          
+          middleLayer.set(i, new MiddleLayer(nummiddleneurons[i], middleAcFnc[i], middleLayer.get(i - 1).getNumNeurons()));
+        }
+        
+        catch(IndexOutOfBoundsException iobe) {
+          
+          middleLayer.add(new MiddleLayer(nummiddleneurons[i], middleAcFnc[i], middleLayer.get(i - 1).getNumNeurons()));
+        }
+        
+        middleLayer.get(i - 1).setNextLayer(middleLayer.get(i));
+      }
+      
+      if(numMiddleLayers > 0) {
+        
+        outputLayer = new OutputLayer(numoutputs, outputAcFnc, middleLayer.get(numMiddleLayers - 1).getNumNeruons());
+        middleLayer.get(numMiddleLayers - 1).setNextLayer(outputLayer);
+      }
+      
+      else  {
+        
+        outputLayer = new OutputLayer(numoutputs, outputAcFnc, numinputs);
+        inputLayer.setNextLayer(outputLayer);
+      }
     }
-    //if the neural net is just the input and output layers, initialize
-    if(numberOfHiddenLayers > 0)  {
-      outputLayer = new OutputLayer(numberofoutputs, outputAcFnc, hiddenLayer.get(numberOfHiddenLayers - 1).getNumberOfNeuronsInLayer());
-      hiddenLayer.get(numberOfHiddenLayers - 1).setNextLayer(outputLayer);
-    }
-    //if there are hidden layers, initialize
-    else  {
-      outputLayer = new OutputLayer(numberofinputs, outputAcFnc, numberofoutputs);
-      inputLayer.setNextLayer(outputLayer);
-    }
+  }
 }
